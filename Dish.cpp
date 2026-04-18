602d9b0f893611c0a1ea846a8ec3893dca97e514
PavlovaSofiiaV <sofapavlova344@gmail.com>
2026-04-18 15:45:46 +0300

    Add new class <Dish>

---
+++ Dish.cpp
@@ -0,0 +1,58 @@
+#include "Dish.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+Dish::Dish() : Dish("Unknown", 0.0, 0.0) {}
+Dish::Dish(string name) : name(name), price(0.0), weight(0.0) {}
+Dish::Dish(string name, double price) {
+    this->name = name;
+    this->price = price;
+    this->weight = 0.0;}
+Dish::Dish(string name, double price, double weight): name(name), price(price), weight(weight) {}
+
+Dish::Dish(const Dish& other) {
+    name=other.name;
+    price=other.price;
+    weight=other.weight;}
+
+Dish::Dish(Dish&& other) {
+    name= std::move(other.name);
+    price = other.price;
+    weight = other.weight;}
+
+Dish::~Dish() {}
+
+Dish Dish::operator+(const Dish& other) {
+    return Dish(name + " and " + other.name, price + other.price, weight + other.weight);
+}
+
+Dish Dish::operator++() {
+    price++;
+    return *this;
+}
+
+ostream& operator<<(ostream& out, const Dish& d) {
+    out << d.name << " | Price: " << d.price << " | Weight: " << d.weight;
+    return out;
+}
+istream& operator>>(istream& in, Dish& d) {
+    in >> d.name >> d.price >> d.weight;
+    return in;
+}
+
+void Dish::printInfo()const {
+    cout << "Dish: " << name << " | Price: " << price << " | Weight: " << weight << endl;
+}
+
+void Dish::show() {
+    cout << "This is dish" << endl;
+}
+double Dish::getprice() const {
+    return price;
+}
+double Dish::getweight() const {
+    return weight;
+}
+string Dish::getname() const {
+    return name;
---
+++ Dish.h
@@ -0,0 +1,36 @@
+#ifndef DISH_H
+#define DISH_H
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Dish{
+protected:
+    string name;
+    double price;
+    double weight;
+public:
+    Dish();
+    Dish(string name);
+    Dish(string name, double price);
+    Dish(string name, double price, double weight);
+    Dish(const Dish& other);
+    Dish( Dish&& other);
+    virtual ~Dish();
+    void printInfo()const ;
+    void show();
+    double getprice()const;
+    double getweight()const;
+    string getname()const;
+
+
+
+    Dish operator+(const Dish& other);
+    Dish operator++();
+    Dish& operator=(const Dish& other)= default;
+
+    friend ostream& operator<<(ostream& out, const Dish& d);
+    friend istream& operator>>(istream& in, Dish& d);
+
+};
+#endif
