a4ec13bf08f7acde44b90fdbc59604685384fa7c
Ruslana <shyshkoruslana08@gmail.com>
2026-04-18 15:46:07 +0300

    add class Customer

---
+++ Customer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ruslana on 18.04.2026.
+//
+
+#include "Customer.h"
---
+++ Customer.h
@@ -0,0 +1,10 @@
+#ifndef LABA5_CUSTOMER_H
+#define LABA5_CUSTOMER_H
+
+
+class Customer {
+
+};
+
+
+#endif //LABA5_CUSTOMER_H
