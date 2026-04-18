bab56fb3b607389ca75a45e5054ff43769db2508
Ruslana <shyshkoruslana08@gmail.com>
2026-04-18 15:48:58 +0300

    add data in class Customer

--- Customer.h
+++ Customer.h
@@ -1,8 +1,36 @@
 #ifndef LABA5_CUSTOMER_H
 #define LABA5_CUSTOMER_H
+#include <string>
+
+using namespace std;
 
 
-class Customer {
+class Customer {private:
+    string name;
+    string surname;
+    string login;
+    int tablenumber;
+    int telnumber;
+    static int count;
+public:
+    Customer();
+    Customer(string name, int tablenumber=1, int telnumber=0);
+    Customer(string name,string surname, string login);
+
+    Customer(const Customer& other);
+    Customer(Customer&& other);
+
+    ~Customer();
+
+    void printOrder();
+
+    void printInfo()const;
+    static int getcount();
+    string getname()const;
+    string getsurname()const;
+    string getlogin()const;
+
+
 
 };
 
